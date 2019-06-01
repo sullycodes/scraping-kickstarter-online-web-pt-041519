48884b4695b0117c65c747dd7fe478bc84277b99
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-01 23:16:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,13 +12,13 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css(".location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
-  project = kickstarter.css("li.project.grid_4")
-  title = project.css("h2.bbcard_name strong a").text
-  loc = project.css(".location-name").text
-  fund = project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+  # project = kickstarter.css("li.project.grid_4")
+  # title = project.css("h2.bbcard_name strong a").text
+  # loc = project.css(".location-name").text
+  # fund = project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
-  p fund
 end
 
 create_project_hash
