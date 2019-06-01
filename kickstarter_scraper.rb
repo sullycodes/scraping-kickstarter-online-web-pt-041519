a85caf4281d16630e63e9b41dea1a32f025e3fe9
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-01 23:00:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   # title = kickstarter.css("li.project.grid_4").first.css(".project-card-wrap").css(".project-card").css("h2.bbcard_name").text
   # print title.gsub!(/\s+/, " ")
   # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
   project = kickstarter.css("li.project.grid_4")
   title = project.css("h2.bbcard_name strong a").text
   p title.split("------")
