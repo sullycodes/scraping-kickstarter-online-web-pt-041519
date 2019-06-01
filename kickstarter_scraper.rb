ff5ad340380c8a2492b1701bc081543f3375c62f
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-01 22:56:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  # title = kickstarter.css("li.project.grid_4").first.css(".project-card-wrap").css(".project-card").css("h2.bbcard_name").text
+  # print title.gsub!(/\s+/, " ")
+  project = kickstarter.css("li.project.grid_4")
+  title = project.css("h2.bbcard_name strong a").text
+  p title.split("------")
 end
+
+create_project_hash
