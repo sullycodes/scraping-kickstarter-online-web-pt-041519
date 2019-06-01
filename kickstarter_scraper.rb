7d7d794dfc8241b99e2423a219479a6e018c8a96
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-01 23:08:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,9 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   project = kickstarter.css("li.project.grid_4")
-  title = project.css("h2.bbcard_name strong a").text
+  title = project.css("h2.bbcard_name strong a").
+  loc = project.css(".").text
+
   p title.split(" - ")
 end
 
